7c4b0aeffcc19a1628d1d27456e300744511b504
grabherr <manfred.grabherr@imbim.uu.se>
2019-01-31 09:10:11 +0100

    Added/changed options in Chromosemble

--- tools/analysis/Chromosemble.cc
+++ tools/analysis/Chromosemble.cc
@@ -35,7 +35,8 @@ int main( int argc, char** argv )
   commandArg<string> outCmmd("-o","output directory");
   commandArg<int> nCmmd("-n","number of CPUs (for full Satsuma run)", 25);
   commandArg<bool> thoroughCmmd("-thorough","runs a full Satsuma alignment (slow!!)", false);
-  commandArg<bool> chrCmmd("-pseudochr","maps scaffolds into chromosomes", false);
+  commandArg<bool> chrCmmd("-nopseudochr","skips mapping scaffolds into chromosomes", false);
+  commandArg<bool> mergeCmmd("-nomerge","skips merging scaffolds", false);
   commandArg<bool> satCmmd("-s","run SatsumaSynteny at the end", false);
 
   commandLineParser P(argc,argv);
@@ -47,6 +48,7 @@ int main( int argc, char** argv )
   P.registerArg(nCmmd);
   P.registerArg(thoroughCmmd);
   P.registerArg(chrCmmd);
+  P.registerArg(mergeCmmd);
   P.registerArg(satCmmd);
   
   P.parse();
@@ -57,7 +59,8 @@ int main( int argc, char** argv )
 
   int nth = P.GetIntValueFor(nCmmd);
   bool bThorough = P.GetBoolValueFor(thoroughCmmd);
-  bool bChr = P.GetBoolValueFor(chrCmmd);
+  bool bNoChr = P.GetBoolValueFor(chrCmmd);
+  bool bNoMerge = P.GetBoolValueFor(mergeCmmd);
   bool bFinal = P.GetBoolValueFor(satCmmd);
 
   int i;
@@ -79,82 +82,83 @@ int main( int argc, char** argv )
   cmd = "mkdir " + outName;
   system(cmd.c_str());
 
-  if (bThorough) {
-    cmd = "SatsumaSynteny2 -q ";
-    cmd += target;
-    cmd += " -t ";
-    cmd += query;
-    cmd += " -slaves ";
-    cmd += Stringify(nth);
-    cmd += " -o ";
-    cmd += outName;
-    cmd += "/xcorr_first";
-    Run(exec_dir, cmd);
-
-  } else {
-    cmd = "mkdir ";
-    cmd += outName;
-    cmd += "/xcorr_first";
-    system(cmd.c_str());
-
-    // FilterGridSeeds
-    cmd = "FilterGridSeeds -q ";
-    cmd += target;
-    cmd += " -t ";
-    cmd += query;
-    cmd += " -o ";
-    cmd += outName;
-    cmd += "/xcorr_first/filterseeds.dat";
-
-    Run(exec_dir, cmd);
-
-    // ChainMatches
-    cmd = "ChainMatches -i ";
-    cmd += outName;
-    cmd += "/xcorr_first/filterseeds.dat -o ";
-    cmd += outName;
-    cmd += "/xcorr_first/filterseeds_chained.dat";
-     
-    Run(exec_dir, cmd);
-    cmd = "MergeXCorrMatches -q ";
-    cmd += target;
-    cmd += " -t ";
-    cmd += query;
-    cmd += " -i ";
-    cmd += outName;
-    cmd += "/xcorr_first/filterseeds_chained.dat";
-    cmd += " -o ";
-    cmd += outName;
-    cmd += "/xcorr_first/satsuma_summary.chained.out";
-
-    Run(exec_dir, cmd);    
-
-  }
+  if (!bNoMerge) {
+    if (bThorough) {
+      cmd = "SatsumaSynteny2 -q ";
+      cmd += target;
+      cmd += " -t ";
+      cmd += query;
+      cmd += " -slaves ";
+      cmd += Stringify(nth);
+      cmd += " -o ";
+      cmd += outName;
+      cmd += "/xcorr_first";
+      Run(exec_dir, cmd);
+      
+    } else {
+      cmd = "mkdir ";
+      cmd += outName;
+      cmd += "/xcorr_first";
+      system(cmd.c_str());
+      
+      // FilterGridSeeds
+      cmd = "FilterGridSeeds -q ";
+      cmd += target;
+      cmd += " -t ";
+      cmd += query;
+      cmd += " -o ";
+      cmd += outName;
+      cmd += "/xcorr_first/filterseeds.dat";
+      
+      Run(exec_dir, cmd);
+      
+      // ChainMatches
+      cmd = "ChainMatches -i ";
+      cmd += outName;
+      cmd += "/xcorr_first/filterseeds.dat -o ";
+      cmd += outName;
+      cmd += "/xcorr_first/filterseeds_chained.dat";
+      
+      Run(exec_dir, cmd);
+      cmd = "MergeXCorrMatches -q ";
+      cmd += target;
+      cmd += " -t ";
+      cmd += query;
+      cmd += " -i ";
+      cmd += outName;
+      cmd += "/xcorr_first/filterseeds_chained.dat";
+      cmd += " -o ";
+      cmd += outName;
+      cmd += "/xcorr_first/satsuma_summary.chained.out";
+      
+      Run(exec_dir, cmd);    
+      
+    }
   
-  cmd = "SortSatsuma -i ";
-  cmd += outName;
-  cmd += "/xcorr_first/satsuma_summary.chained.out > ";
-  cmd += outName;
-  cmd += "/xcorr_first/satsuma_summary.chained.out_sorted";
-  Run(exec_dir, cmd);    
-
-
-  cmd = "MergeScaffoldsBySynteny -i ";
-  cmd += outName;
-  cmd += "/xcorr_first/satsuma_summary.chained.out_sorted -f ";
-  cmd += query;
-  cmd += " -o ";
-  cmd += outName;
-  cmd += "/superscaffolds.fasta";
-  Run(exec_dir, cmd);    
+    cmd = "SortSatsuma -i ";
+    cmd += outName;
+    cmd += "/xcorr_first/satsuma_summary.chained.out > ";
+    cmd += outName;
+    cmd += "/xcorr_first/satsuma_summary.chained.out_sorted";
+    Run(exec_dir, cmd);    
     
-  // Change query name
-  query = outName;
-  query += "/superscaffolds.fasta";
- 
+    
+    cmd = "MergeScaffoldsBySynteny -i ";
+    cmd += outName;
+    cmd += "/xcorr_first/satsuma_summary.chained.out_sorted -f ";
+    cmd += query;
+    cmd += " -o ";
+    cmd += outName;
+    cmd += "/superscaffolds.fasta";
+    Run(exec_dir, cmd);    
+    
+    // Change query name
+    query = outName;
+    query += "/superscaffolds.fasta";
+  }
 
   //=======================================================
-  if (bChr) {
+  if (!bNoChr) {
 
     if (bThorough) {
       cmd = "SatsumaSynteny2 -q ";
